3513149eb3b222c99dbda697935a31ce53597bfe
web-je <58517997+web-je@users.noreply.github.com>
2020-10-19 16:36:28 +0900

    Add files via upload

---
+++ RattaDoEat/src/main/java/koitt/ratta/rde/RattaDoEatApplication.java
@@ -0,0 +1,13 @@
+package koitt.ratta.rde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RattaDoEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RattaDoEatApplication.class, args);
+	}
+
+}
